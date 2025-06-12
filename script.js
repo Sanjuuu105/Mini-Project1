ed86f37d0ff06a1d51ff2ac0b34d87c72ceedde3
Sanjana Kabara <sanjanakabara@gmail.com>
2025-06-12 18:50:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function beforesubmit(){
+    let inputdate = document.querySelector(".inputdate");
+    let outputdate = document.querySelector(".outputdate");
+    console.log("inputValue :", inputdate.value);
+
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
+}
