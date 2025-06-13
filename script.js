bf6a960d1118382e122b128183e29818b580b899
Sanjana Kabara <sanjanakabara@gmail.com>
2025-06-13 16:04:18 +0530

    recaptcha validation

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function beforesubmit(){
+let captchacheck =  false;
+function beforesubmit(event){
+    if(captchacheck){
     let inputdate = document.querySelector(".inputdate");
     let outputdate = document.querySelector(".outputdate");
     console.log("inputValue :", inputdate.value);
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formattedDate;
+    }else{
+        alert("Please check the reCaptcha box to submit the lead");
+        event.preventDefault();
+    }
+    
 }
 
 
@@ -17,3 +24,7 @@ function timestamp()
             document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
         }
 } setInterval(timestamp, 500); 
+
+function captchasuccess(){
+    captchacheck = true;
+}
